d6059bd402edc1030340e79f5904c13132da2515
Prithvi Bose <prithvibose95@gmail.com>
2024-03-09 19:39:47 +0530

    added standalone components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '',pathMatch:'full', redirectTo: 'homePage' }, {path:'homePage', component:HomePageComponent}];
